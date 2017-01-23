78a624614d43f15e68f3167a952c9dfba2b98fa5
alexanderkozhevin <turboasm21@gmail.com>
2017-01-23 12:18:55 +0300

    addd progress bad for the table, attached firebase, creating new item, removing item

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -1,4 +1,4 @@
-angular.module("bookapp").controller("ProjectsCtrl", function($scope, $firebaseObject, $firebaseAuth, $firebaseArray) {
+angular.module("bookapp").controller("ProjectsCtrl", function($scope, $firebaseObject, $firebaseAuth, $firebaseArray, $mdToast) {
 
 	$scope.authObj = $firebaseAuth();
 	var firebaseUser = $scope.authObj.$getAuth();
@@ -7,6 +7,8 @@ angular.module("bookapp").controller("ProjectsCtrl", function($scope, $firebaseO
 	var list = $firebaseArray(listref);
 	$scope.list = list;
 
+	$scope.promise = $scope.list.$loaded()
+	console.log($scope.promise)
 	$scope.showInput = false
 
 	$scope.toggleInput = function(){
@@ -15,20 +17,37 @@ angular.module("bookapp").controller("ProjectsCtrl", function($scope, $firebaseO
 		setTimeout(function(){
 			doc.focus()
 		})
-
 	}
 
-	$scope.selected = []
-	$scope.data  = [
-		{name: "project1",
-		 value: 32
-		 }, {
-		 	name: "project2",
-		 	value: 12
-		 }, {
-		 	name: "project3",
-		 	value: 88
-		 }
-	]
+	$scope.handleInput = function(e){
+		switch (e.keyCode){
+			case 27:
+				$scope.showInput = false;
+				break;
+			case 13:
+				if ($scope.newItemName.length>4){
+				   list.$add({ name: $scope.newItemName })
+					 $scope.newItemName = ""
+					 $scope.showInput = false;
+				} else {
+					$mdToast.show(
+		        $mdToast.simple().textContent('Name is too short').position('bottom right').hideDelay(3000)
+		      );
+				}
+				break;
+		}
+	}
+
+	$scope.removeItem = function(item){
+		var name = item.name;
+		list.$remove(item).then(function(ref) {
+		  // ref.key === item.$id; // true
+			$mdToast.show(
+				$mdToast.simple().textContent(`'${name}' has been removed`).position('bottom right').hideDelay(3000)
+			);
+		});
+	}
+
+
 
 })
