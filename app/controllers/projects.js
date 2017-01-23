7c1ce757e6fd8f767d54e6a22f022039172db048
alexanderkozhevin <turboasm21@gmail.com>
2017-01-23 11:50:30 +0300

    added input field for the new item

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -1,15 +1,34 @@
-angular.module("bookapp").controller("ProjectsCtrl", function($scope, $firebaseObject, $firebaseAuth) {
+angular.module("bookapp").controller("ProjectsCtrl", function($scope, $firebaseObject, $firebaseAuth, $firebaseArray) {
 
-$scope.data  = [
-	{name: "project1",
-	 value: 32
-	 }, {
-	 	name: "project2",
-	 	value: 12
-	 }, {
-	 	name: "project3",
-	 	value: 88
-	 }
-]
+	$scope.authObj = $firebaseAuth();
+	var firebaseUser = $scope.authObj.$getAuth();
+	var ref = firebase.database().ref(firebaseUser.uid);
+	var listref = ref.child("projects")
+	var list = $firebaseArray(listref);
+	$scope.list = list;
+
+	$scope.showInput = false
+
+	$scope.toggleInput = function(){
+		$scope.showInput = true
+		var doc = document.querySelector('.input-field')
+		setTimeout(function(){
+			doc.focus()
+		})
+
+	}
+
+	$scope.selected = []
+	$scope.data  = [
+		{name: "project1",
+		 value: 32
+		 }, {
+		 	name: "project2",
+		 	value: 12
+		 }, {
+		 	name: "project3",
+		 	value: 88
+		 }
+	]
 
 })
