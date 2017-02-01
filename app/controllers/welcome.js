f9f57fbedd28c572b0a5a7709e26113114eb0f7b
alexanderkozhevin <turboasm21@gmail.com>
2017-02-01 12:11:11 +0300

    worldcat - code for heroku server

--- app/controllers/welcome.js
+++ app/controllers/welcome.js
@@ -1,4 +1,4 @@
-angular.module("bookapp").controller("WelcomeCtrl", function($scope, $firebaseObject, $firebaseAuth, $http) {
+angular.module("bookapp").controller("WelcomeCtrl", function($scope, $firebaseObject, $firebaseAuth, $http, $sce) {
 
 
   $scope.authObj = $firebaseAuth();
@@ -22,22 +22,24 @@ angular.module("bookapp").controller("WelcomeCtrl", function($scope, $firebaseOb
 
   }
 
-  $scope.request =function(){
+  $scope.request = function(){
 
-    
 
-     // $http({
-     //    method: 'GET',
-     //    url: 'http://www.worldcat.org/webservices/catalog/search/worldcat/opensearch?q=mechanics&wskey=qKbgmXudRlrZHBV8sPV6Jw1lOhIYG5HP5ZU56GkiOsKXjlj95Bvt8nqc0d6pc00tnfbUm6C7mI6ALO8i'
-     //  }).then(function successCallback(response) {
-        
-     //    var x2js = new X2JS();
-     //    $scope.finaldata = x2js.xml_str2json(response.data);
-     //    console.log($scope.finaldata);
-        
-     //    }, function errorCallback(response) {
-     //      console.log(response)
-     //    });
+
+     $http({
+        method: 'GET',
+        url: 'https://wordcat.herokuapp.com/'
+      }).then(function successCallback(response) {
+        console.log('yes')
+        console.log(response)
+        //
+        // var x2js = new X2JS();
+        // $scope.finaldata = x2js.xml_str2json(response.data);
+        // console.log($scope.finaldata);
+
+        }, function errorCallback(response) {
+          console.log(response)
+        });
   }
 
 })
